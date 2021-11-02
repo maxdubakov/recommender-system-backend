4cf542d84189abad2ab7baa8621559802ccbc079
Max-7th-byte <max.dubakov@gmail.com>
2021-11-02 20:48:53 +0100

    Populated User-Beer table

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index),
     path('populate-users', views.populate_users),
     path('populate-users-beers', views.populate_users_beers),
+    path('get-user-beers/<slug:slug>', views.get_beers_for_user),
 ]
--- user/views.py
+++ user/views.py
@@ -3,6 +3,7 @@ import pickle as pkl
 from django.http import HttpResponse
 import pandas as pd
 
+from beer.models import Beer
 from user.models import User
 
 data_path = './data/beer_new_reviews.csv'
@@ -24,4 +25,22 @@ def populate_users(request):
     return HttpResponse('All Good')
 
 def populate_users_beers(request):
+    data = pd.read_csv(data_path)
+
+    users = User.objects.all()
+    number_of_users = len(users)
+    for i, user in enumerate(users):
+        if i >= 100:
+            break
+        beer_ids = list(data['beer_beerid'][data['review_profilename'] == user.name].unique())
+        print(f'Populating {len(beer_ids)} beers for user {user} ({i}/{number_of_users})')
+        for beer_id in beer_ids:
+            beer = Beer.objects.get(id=beer_id)
+            user.beers.add(beer)
+
     return HttpResponse('All Good')
+
+
+def get_beers_for_user(request, slug):
+    user = User.objects.get(name=slug)
+    return HttpResponse(f'All Good:\n {[beer.name for beer in user.beers.all()]}\n\n{user.name}\n\n {len(user.beers.all())}')
